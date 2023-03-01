c29a9267fe44336898ff0a4487f8da3b47110ce0
Ananththandavarayamurthi <ananthcivil9876@gmail.com>
2023-03-01 21:40:11 +0530

    cors

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ import calculation from "./routes/calculation.js";
 
 // MiddleWare
 app.use(express.json());
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "welcome" });

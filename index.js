6c8aa5fafad88a02d17a02aaae10719b71b55e45
Ananththandavarayamurthi <110927979+Ananththandavarayamurthi@users.noreply.github.com>
2023-03-01 21:57:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ import calculation from "./routes/calculation.js";
 
 // MiddleWare
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.json({ message: "welcome" });
